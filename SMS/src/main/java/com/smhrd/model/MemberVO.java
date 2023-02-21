a7f274f5b76c55de323fe8c4fdfb49544f3093de
smhrd <smhrd@DESKTOP-IIGME00>
2023-02-21 16:58:47 +0900

    daovo기본

---
+++ SMS/src/main/java/com/smhrd/model/MemberVO.java
@@ -0,0 +1,86 @@
+package com.smhrd.model;
+
+public class MemberVO {
+	
+	private String userId;
+	private String userPw;
+	private String userTel;
+	private String userName;
+	
+	
+	
+	// 회원가입 0221
+	public MemberVO(String userId, String userPw, String userTel, String userName) {
+		super();
+		this.userId = userId;
+		this.userPw = userPw;
+		this.userTel = userTel;
+		this.userName = userName;
+	}
+
+
+
+	private String getUserId() {
+		return userId;
+	}
+
+
+
+	private void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+
+
+	private String getUserPw() {
+		return userPw;
+	}
+
+
+
+	private void setUserPw(String userPw) {
+		this.userPw = userPw;
+	}
+
+
+
+	private String getUserTel() {
+		return userTel;
+	}
+
+
+
+	private void setUserTel(String userTel) {
+		this.userTel = userTel;
+	}
+
+
+
+	private String getUserName() {
+		return userName;
+	}
+
+
+
+	private void setUserName(String userName) {
+		this.userName = userName;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
